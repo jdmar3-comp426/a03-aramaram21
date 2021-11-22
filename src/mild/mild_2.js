e280230d87f338a6d9157353bd1bdb6342652e11
aramaram21 <aramaram@live.unc.edu>
2021-11-22 16:02:31 -0500

    fourth  mild2

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -76,7 +76,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+    const { [key]:omit, ...object } = object;
 }
 
 /**
