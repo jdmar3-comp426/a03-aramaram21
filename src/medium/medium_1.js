35f04a5d1d36f0ad0b54fffde99aed3c30065cb6
aramaram21 <aramaram@live.unc.edu>
2021-11-22 16:54:32 -0500

    fourth  mild2

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,7 +8,11 @@ import {variance} from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
-
+   let sm = 0; 
+   for( let i = 0; i < array.length; i++){
+       sm += array[i];
+   }
+   return sm;
 }
 
 
