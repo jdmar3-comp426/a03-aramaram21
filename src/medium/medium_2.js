c235557653ed52d8a1c51169cf99bf6714819cbb
aramaram21 <aramaram@live.unc.edu>
2021-11-24 20:53:00 -0500

    fourth  mild2

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -20,7 +20,7 @@ see under the methods section
  * @param {allCarStats.ratioHybrids} ratio of cars that are hybrids
  */
 export const allCarStats = {
-    avgMpg: {city: (mpg_data["city_mpg"]), highway: (mpg_data["highway_mpg"]/mpg_data.length)},
+    avgMpg: {city: (mpg_data.reduce((a,b) => a + (b["city_mpg"] || 0),0)/mpg_data.length), highway: (mpg_data["highway_mpg"]/mpg_data.length)},
     allYearStats: undefined,
     ratioHybrids: undefined,
 };
